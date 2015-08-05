b4cee2f7c93eec923ae1aa281148dfc3509aeab9
Blake Embrey <hello@blakeembrey.com>
2015-08-05 15:37:18 -0700

    Accept single function and use flattened stack

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,32 @@
 import flatten = require('array-flatten')
 
-export type Middleware = (req: any, res: any, next: (err?: Error) => any) => any
+export type Middleware = (req?: any, res?: any, next?: (err?: Error) => any) => any
 
-export function compose (handlers: Middleware[]): Middleware {
-  var stack = flatten(handlers)
+export function compose (handlers: Middleware | Middleware[]): Middleware {
+  if (typeof handlers === 'function') {
+    return <Middleware> handlers
+  }
+
+  const stack = flatten(<Middleware[]> handlers)
 
   // Quick exit.
   if (handlers.length < 2) {
-    return handlers[0]
+    return stack[0]
   }
 
   return function middleware (req, res, done) {
-    var index = 0
+    let index = 0
 
     function next (err?: Error): void {
       if (err) {
         return done(err)
       }
 
-      if (index === handlers.length) {
+      if (index === stack.length) {
         return done()
       }
 
-      var handler = handlers[index++]
+      const handler = stack[index++]
 
       return handler(req, res, next)
     }
