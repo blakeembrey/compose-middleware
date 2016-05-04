3a479b6817c1ddbcae1852290cda0209880c0f76
Blake Embrey <hello@blakeembrey.com>
2016-05-04 09:45:48 -0700

    Remove typings/

--- src/typings/array-flatten.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'array-flatten' {
-  interface NestedArray <T> {
-    [index: number]: T | NestedArray<T>
-  }
-
-  function arrayFlatten <T> (array: NestedArray<T>): T[]
-
-  export = arrayFlatten
-}
